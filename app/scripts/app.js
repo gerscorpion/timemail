ae50369dc2b9e138fa92b58d2294d5f008839d58
Luis Amoroso <luisamoroso@Luiss-MacBook-Pro.fritz.box>
2015-03-01 17:50:00 +0100

    added folder structure

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name timemailApp
+ * @description
+ * # timemailApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('timemailApp', [
+    'ngMessages'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timemailApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the timemailApp
+ */
+angular.module('timemailApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
